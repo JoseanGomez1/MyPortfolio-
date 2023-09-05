8dff60ce9462ec9e1c79d6136acac85c9c78ec26
JoseanGomez1 <joseangomez123@gmail.com>
2023-09-04 23:34:01 -0700

    last

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,11 +14,15 @@ function About() {
     getAboutData();
   }, []);
 
+
   const loaded = () => (
     <div className="about-container">
-      <h2>{about.name}</h2>
+      <img src={about.headshot} alt={about.name} className="about-image" />
+     <h2>{about.name}</h2>
       <h3>{about.email}</h3>
+      <h4>{about.linkedin}</h4>
       <p>{about.bio}</p>
+      <a href={about.resume} target="_blank" rel="noopener noreferrer">Download Resume (PDF)</a>
     </div>
   );
 
